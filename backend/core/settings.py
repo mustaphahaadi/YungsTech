38d8de58b3cfd12a2e9026511afbffbd70fe5a99
mustaphahaadi <mustaphahaadi04@gmail.com>
2025-05-11 19:19:30 +0000

    chore: run migrations

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-y7t2^h3!v@8f$_s9&4@j2x#n*=ug!+$p7=@!&=6=+$p7=@!&=6=')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
--- backend/manage.py
+++ backend/manage.py
@@ -5,6 +5,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Add the current directory to the Python path
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, current_path)
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
